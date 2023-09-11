3eebed1d737eedf3a140cdc0f9ff5ed278fb259e
diyorarajabova <diyoraabd04@gmail.com>
2023-09-11 12:35:02 -0400

    Hooks

---
+++ tests/authWithHooks.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+test.describe('Authentication & Authorization', () => {
+    test.beforeEach(async({page}) =>{
+        await page.goto('https://coding.pasv.us/user/login');
+    })
+    test('Sign in with existing credentials', async ({page})=>{
+        await page.locator("#normal_login_email").type('diyoraabd04@gmail.com');
+        await page.locator('#normal_login_password').fill('job0404di');
+        await page.locator('button[type="submit"]').click()
+        expect(page.locator('ant-avatar-square'))
+    })
+    test('Sing in with invalid credentials', async ({page})=>{
+        await page.locator("#normal_login_email").type('diyoraabd@gmail.com');
+        await page.locator('#normal_login_password').fill('job0404');
+        await page.locator('button[type="submit"]').click()
+        //expect(page.locator('.ant-notification-notice-error'))
+        const toast = page.locator('.ant-notification-notice-message')
+        await expect(toast).toBeVisible()
+        await expect(toast).toHaveText('User login. Fail')    })
+})
