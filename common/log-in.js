0cee2527f32c3309b9a32ea60999d39992676b42
diyorarajabova <diyoraabd04@gmail.com>
2023-09-11 12:48:54 -0400

    Added dotEnv

--- common/log-in.js
+++ common/log-in.js
@@ -1,5 +1,5 @@
 export const logIn= async()=> {
-    await page.goto('https://coding.pasv.us/user/login');
+    await page.goto('user/login');
 
     await page.locator("#normal_login_email").type(email);
     await page.locator('#normal_login_password').fill(password);
